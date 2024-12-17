4899fa8b493b201d2c27ed54a7b7699e7469b966
Ephy Kimotho <ephyk48@gmail.com>
2024-12-17 20:01:28 +0300

    Created StudenClass class withs its interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,28 +16,32 @@ interface printTeacherFunction {
     (firstName: string, lastName: string): string
 }
 
-const teacher3: Teacher = {
-    firstName: 'John',
-    fullTimeEmployee: false,
-    lastName: 'Doe',
-    location: 'London',
-    contract: false,
-}
-
-console.log(teacher3);
-
-const director1: Directors = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-};
-console.log(director1);
-
 
 const printTeacher: printTeacherFunction = function (firstName, lastName) {
     return `${firstName.at(0)}. ${lastName}`
 }
 
-console.log(printTeacher("John", "Doe"))
+interface Student {
+    firstName: string;
+    lastName: string;
+    workOnHomework: () => string;
+    displayName: () => string;
+}
+
+class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework() {
+        return "Currently working"
+    }
+
+    displayName() {
+        return this.firstName
+    }
+}
